bb808fd3394906119073e744474f00603f076ca1
vincent <vincent.picardpro@gmail.com>
2020-11-09 14:21:41 +0100

    http-collect pipe+bl

--- http-collect.js
+++ http-collect.js
@@ -1,10 +1,21 @@
 const http = require('http')
-let result = ""
-http.get(process.argv[2],(response) =>{
-    response.on('data', (data)=> {
-        result += data.toString()
-    })
-    response.on('end', () =>{
-        console.log(result.length + '\n' + result)
-    })
-}) 
+const bl = require('bl')
+//let result = ""
+//http.get(process.argv[2],(response) =>{
+//   response.on('data', (data)=> {
+//       result += data.toString()
+//   })
+//   response.on('end', () =>{
+//     console.log(result.length + '\n' + result)
+//  })
+//}) 
+
+http.get(process.argv[2], function (response) {
+    response.pipe(bl(function (err, data) {
+        if (err) {
+           console.error(err) 
+        }
+        console.log(data.toString().length+'\n'+data.toString())
+
+    }))
+})
